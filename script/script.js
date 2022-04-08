7ef9aeb294328007cae23c14b3005f5a7527f422
Lucas Roman <lucas.e.roman@gmail.com>
2022-04-07 20:03:48 -0700

    simplied html to work on java functionality

--- script/script.js
+++ script/script.js
@@ -1,12 +1,25 @@
+//display current date 
 
-var 
+var textAreaEl = $('#input');
+var hourEL = $('#hour');
+var saveBtnEl = $('saveBtn');
 
-function displayTime() {
-    var currentTime = moment().format('dddd, MMMM Do');
-    
-    
-    console.log(currentTime)
+ var todayDate = moment();
+ $('#currentDay').text(todayDate.format('dddd, MMMM Do'));
 
-}
 
-displayTime()
+ function saveSchedule() {
+    console.log("im linked!")
+};
+
+saveBtnEl.click(saveSchedule())
+
+//figure out if in past present of future
+    //comapare slot time to current time if >=< 
+    //change textbox color past to red
+    //change textbox color current to light blue
+    //change textbox color future to light green
+
+
+var schedTime = moment().format('HH');
+console.log(schedTime)
